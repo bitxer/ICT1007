d3a9e8658512db74753394ea11f53153e8622596
Joshua Lee <33726740+joshualeejunyi@users.noreply.github.com>
2021-02-19 17:31:14 +0800

    Update algo.h

--- algo.h
+++ algo.h
@@ -16,8 +16,8 @@ int DEBUG_MODE = 0;
 int K_INCREMENT = 0;
 
 void printHelp(const char* name, const char* option);
+ProcessList* getTestData();
 void sortedInsert(ProcessList** head, ProcessList* node);
 ProcessList* createProcess(int process_id, int arrival_time, int burst_time, int waiting_time, int turnaround_time);
-ProcessList* getTestData();
 void printProcessList(ProcessList*  n);
 void runProcesses(ProcessList* processes, int* time, int* k_factor);
