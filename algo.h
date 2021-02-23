5767ae6c7643fd363c7d92bf4b19da1657469e46
Joshua Lee <33726740+joshualeejunyi@users.noreply.github.com>
2021-02-23 17:36:16 +0800

    Update algo.h

--- algo.h
+++ algo.h
@@ -3,6 +3,13 @@
 #include <unistd.h>
 #include <string.h>
 
+#define __DEBUG__
+#ifdef __DEBUG__
+#define DBG(...) printf(__VA_ARGS__);
+#else
+#define DBG(...) ;
+#endif
+
 typedef struct Process {
     int process_id;
     int arrival_time;
@@ -14,10 +21,12 @@ typedef struct Process {
 
 int DEBUG_MODE = 0;
 int K_INCREMENT = 0;
+int BURST_TIME = 1;
+int ARRIVAL_TIME = 2;
 
 void printHelp(const char* name, const char* option);
-ProcessList* getTestData();
-void sortedInsert(ProcessList** head, ProcessList* node);
+int getTestData(char* filepath, int* num_of_processes, ProcessList** process_list, ProcessList** ready_queue, ProcessList** incoming_arrivals) ;
+void sortedInsert(ProcessList** head, ProcessList* node, int sortBy);
 ProcessList* createProcess(int process_id, int arrival_time, int burst_time, int waiting_time, int turnaround_time);
 void printProcessList(ProcessList*  n);
-void runProcesses(ProcessList* processes, int* time, int* k_factor);
+void runProcesses(ProcessList* processes, int* time, int* k_factor, int* total_num_of_processes);
