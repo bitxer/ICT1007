4fb8a4d420be8b3254e18da18de45f7b1eb0f488
Joshua Lee <33726740+joshualeejunyi@users.noreply.github.com>
2021-02-23 17:36:04 +0800

    Update algo.c

--- algo.c
+++ algo.c
@@ -1,22 +1,43 @@
 #include "algo.h"
 #include "getopt.h"
 
-void sortedInsert(ProcessList** head, ProcessList* node) {
+void sortedInsert(ProcessList** head, ProcessList* node, int sortBy) {
     if (*head == NULL) {
         *head = node;
         return;
     }
 
     ProcessList* current = *head;
+    int node_field = 0;
+    int current_field = 0;
+    int next_field = 0;
 
-    if (node->burst_time < current->burst_time) {
+    if (sortBy == BURST_TIME) {
+        node_field = node->burst_time;
+        current_field = current->burst_time;
+        if (current->next != NULL) {
+            next_field = current->next->burst_time;
+        }
+    } else if (sortBy == ARRIVAL_TIME) {
+        node_field = node->arrival_time;
+        current_field = current->arrival_time;
+        if (current->next != NULL) {
+            next_field = current->next->arrival_time;
+        }
+
+    } else {
+        printf("help la what you doing\n");
+        return;
+    }
+
+    if (node_field < current_field) {
         node->next = current;
         *head = node;
         return;
     }
 
     while (current != NULL) {
-        if (node->burst_time >= current->burst_time && (current->next == NULL || node->burst_time < current->next->burst_time)) {
+        if (node_field >= current_field && (current->next == NULL || node_field < next_field)) {
             node->next = current->next;
             current->next = node;
             return;
@@ -40,33 +61,83 @@ ProcessList* createProcess(int process_id, int burst_time, int arrival_time, int
 
 void printProcessList(ProcessList*  n) {
     while (n != NULL) {
-        if (n->next->next == NULL) {
-            printf("P%d -> P%d\n", n->process_id, n->next->process_id);
-            return;
-        } else {
-            printf("P%d -> ", n->process_id);
-        }
-
+        printf("P%d -> ", n->process_id);
         n = n->next;
     }
 }
 
-void runProcesses(ProcessList* processes, int* time, int* k_factor) {
+void runProcesses(ProcessList* processes, int* time, int* k_factor, int* total_num_of_processes) {
     int num_of_processes = 0;
 
-    if (processes == NULL) {
-        printf("No processes in waiting queue.\n");
-        return;
-    }
+    printf("time: %d\n", *time);
 
-    ProcessList* previous = NULL;
+    for (*time; num_of_processes < *total_num_of_processes; *time++) {
+        
+        if (processes == NULL) {
+            printf("Time passed:%d\n", *time);
+            printf("No process to run\n");
+            continue;
+        }
 
-    while (processes != NULL) {
-        printf("======================\n");
-        printf("Time passed: %d\n", *time);
-        if (num_of_processes == 0) {
+        ProcessList* previous = NULL;
+
+        while (processes != NULL) {
+            printf("======================\n");
+            printf("Time passed: %d\n", *time);
+            if (num_of_processes == 0) {
+                printf("Running PID %d for %d cycles\n", processes->process_id, processes->burst_time);
+
+                *time += processes->burst_time;
+                processes->turnaround_time = *time - processes->arrival_time;
+                processes->waiting_time = processes->turnaround_time - processes->burst_time;
+                printf("Turnaround time for PID %d: %d\n", processes->process_id, processes->turnaround_time);
+                printf("Waiting time for PID %d: %d\n", processes->process_id, processes->waiting_time);
+                previous = processes;
+                processes = processes->next;
+                num_of_processes++;
+                continue;
+            }
+
+            // increment K factor alternatively
+            if (K_INCREMENT == 1) {
+                if (num_of_processes % 2 == 0) {
+                    *k_factor = *k_factor + 1;
+                }
+            }
+            
+
+            if (processes->next != NULL) {
+                ProcessList* ProcessReadyOne = processes;
+                ProcessList* ProcessReadyTwo = processes->next;
+
+                int first = *k_factor * ProcessReadyOne->burst_time;
+                int second = *time + ProcessReadyTwo->burst_time;
+
+                if (DEBUG_MODE) {
+                    printf("first ready process burst time: %d\n", ProcessReadyOne->burst_time);
+                    printf("second ready process burst time: %d\n", ProcessReadyTwo->burst_time);
+                    printf("k factor: %d\n", *k_factor);
+                    printf("(k * first): %d\n", first);
+                    printf("(t + second): %d\n", second);
+                    printf("first > second: %d\n", first > second);
+                    if (first > second) {
+                        printf("CPU goes to second process P%d\n", ProcessReadyTwo->process_id);
+                    } else {
+                        printf("CPU goes to first process P%d\n", ProcessReadyOne->process_id);
+                    }
+                }
+
+                if (first > second) {
+                    if (previous != NULL) {
+                        previous->next = ProcessReadyTwo;
+                    }
+                    ProcessReadyOne->next = ProcessReadyTwo->next;
+                    ProcessReadyTwo->next = ProcessReadyOne;
+                    processes = ProcessReadyTwo;
+                }
+            }
+            
             printf("Running PID %d for %d cycles\n", processes->process_id, processes->burst_time);
-
             *time += processes->burst_time;
             processes->turnaround_time = *time - processes->arrival_time;
             processes->waiting_time = processes->turnaround_time - processes->burst_time;
@@ -75,66 +146,16 @@ void runProcesses(ProcessList* processes, int* time, int* k_factor) {
             previous = processes;
             processes = processes->next;
             num_of_processes++;
-            continue;
+            
+            printf("======================\n");
         }
-
-        // increment K factor alternatively
-        if (K_INCREMENT == 1) {
-            if (num_of_processes % 2 == 0) {
-                *k_factor = *k_factor + 1;
-            }
-        }
-        
-
-        if (processes->next != NULL) {
-            ProcessList* ProcessReadyOne = processes;
-            ProcessList* ProcessReadyTwo = processes->next;
-
-            int first = *k_factor * ProcessReadyOne->burst_time;
-            int second = *time + ProcessReadyTwo->burst_time;
-
-            if (DEBUG_MODE) {
-                printf("first ready process burst time: %d\n", ProcessReadyOne->burst_time);
-                printf("second ready process burst time: %d\n", ProcessReadyTwo->burst_time);
-                printf("k factor: %d\n", *k_factor);
-                printf("(k * first): %d\n", first);
-                printf("(t + second): %d\n", second);
-                printf("first > second: %d\n", first > second);
-                if (first > second) {
-                    printf("CPU goes to second process P%d\n", ProcessReadyTwo->process_id);
-                } else {
-                    printf("CPU goes to first process P%d\n", ProcessReadyOne->process_id);
-                }
-            }
-
-            if (first > second) {
-                if (previous != NULL) {
-                    previous->next = ProcessReadyTwo;
-                }
-                ProcessReadyOne->next = ProcessReadyTwo->next;
-                ProcessReadyTwo->next = ProcessReadyOne;
-                processes = ProcessReadyTwo;
-            }
-        }
-        
-        printf("Running PID %d for %d cycles\n", processes->process_id, processes->burst_time);
-        *time += processes->burst_time;
-        processes->turnaround_time = *time - processes->arrival_time;
-        processes->waiting_time = processes->turnaround_time - processes->burst_time;
-        printf("Turnaround time for PID %d: %d\n", processes->process_id, processes->turnaround_time);
-        printf("Waiting time for PID %d: %d\n", processes->process_id, processes->waiting_time);
-        previous = processes;
-        processes = processes->next;
-        num_of_processes++;
-        
-        printf("======================\n");
     }
 
     printf("Number of processes ran: %d\n", num_of_processes);
     
 }
 
-ProcessList* getTestData(char* filepath) {
+int getTestData(char* filepath, int* num_of_processes, ProcessList** process_list, ProcessList** ready_queue, ProcessList** incoming_arrivals) {
     FILE* fp;
     char* line = NULL;
     size_t len = 0;
@@ -144,29 +165,35 @@ ProcessList* getTestData(char* filepath) {
     int arrival_time = 0;
     int burst_time = 0;
 
-    ProcessList* head = NULL;
-
     if (filepath == NULL) {
         filepath = "./data.csv";
     }
 
     fp = fopen(filepath, "r");
     if (fp == NULL)
-        return NULL;
+        return -1;
 
     while ((read = getline(&line, &len, fp)) != -1) {
         sscanf(line, "%d, %d, %d\n", &process_id, &burst_time, &arrival_time);
 
         ProcessList* newProc = NULL;
         newProc = createProcess(process_id, burst_time, arrival_time, 0, 0);
-        sortedInsert(&head, newProc);
+
+        sortedInsert(process_list, newProc, ARRIVAL_TIME);
+
+        if (arrival_time == 0) {
+            sortedInsert(ready_queue, newProc, ARRIVAL_TIME);
+        } else {
+            sortedInsert(incoming_arrivals, newProc, BURST_TIME);
+        }
+        *num_of_processes = *num_of_processes + 1;
     }
 
     fclose(fp);
     if (line)
         free(line);
     
-    return head;
+    return 0;
 }
 
 void printHelp(const char* name, const char* option) {
@@ -174,7 +201,7 @@ void printHelp(const char* name, const char* option) {
     printf("usage:\t%s [flags]\n", name);
     printf("-d\tdebug mode\n");
     printf("-h\tprint this help menu\n");
-    printf("-i\tspecify input filepath");
+    printf("-i\tspecify input filepath\n");
     printf("-k\tincreases k factor on the alternate loops\n");
     return;
 }
@@ -183,6 +210,7 @@ int main(int argc, char* argv[]) {
     int k_factor = 2;
     int time = 0;
     int opt = 0;
+    int total_num_of_processes = 0;
     char* filepath = NULL;
 
     if (argc > 1) {
@@ -193,7 +221,7 @@ int main(int argc, char* argv[]) {
                     break;
                 case 'h':
                     printHelp(argv[0], argv[1]);
-                    break;
+                    return 0;
                 case 'k':
                     K_INCREMENT = 1;
                     break;
@@ -207,22 +235,27 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    int resp = 0;
     ProcessList* process_list = NULL;
+    ProcessList* ready_queue = NULL;
+    ProcessList* incoming_arrivals = NULL;
 
+    resp = getTestData(filepath, &total_num_of_processes, &process_list, &ready_queue, &incoming_arrivals);
 
-    process_list = getTestData(filepath);
-
-    if (process_list == NULL) {
+    if (resp != 0) {
         printf("unable to open file: %s\n", filepath);
         return -1;
     }
 
+    printf("number of data: %d\n", total_num_of_processes);
+
+
     printf("SJF Process List: ");
 
     printProcessList(process_list);
-    runProcesses(process_list, &time, &k_factor);
+    runProcesses(process_list, &time, &k_factor, &total_num_of_processes);
     
-    printf("Resultant process order: ");
-    printProcessList(process_list);
+    // printf("Resultant process order: ");
+    // printProcessList(process_list);
 
 }
