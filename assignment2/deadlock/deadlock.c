cea2690ad61264acef68b3723f75f89aa2a388ed
joshualeejunyi <joshua.lee.99@hotmail.com>
2021-03-30 04:44:56 -0700

    love u harish

--- assignment2/deadlock/deadlock.c
+++ assignment2/deadlock/deadlock.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <unistd.h>
+#include <time.h>
 
 // global mutex x5
 pthread_mutex_t first_mutex, second_mutex, third_mutex, fourth_mutex, fifth_mutex;
@@ -11,12 +12,13 @@ pthread_mutex_t first_mutex, second_mutex, third_mutex, fourth_mutex, fifth_mute
 // create a struct cause dynamic
 typedef struct mutex_map_struct {
     int key;
-    pthread_mutex_t mutex;
+    pthread_mutex_t *mutex;
     struct mutex_map_struct *next;
 } mutex_map;
 
+
 typedef struct thread_params_struct {
-    mutex_map *map;
+    mutex_map **map;
     mutex_map **head;
 } thread_params;
 
@@ -26,11 +28,11 @@ thread_params *createMutexMap(mutex_map** dict, int key, pthread_mutex_t* mutex,
     assert(resp == 0);
     *dict = malloc(sizeof(struct mutex_map_struct));
     (*dict)->key = key;
-    (*dict)->mutex = *mutex;
+    (*dict)->mutex = mutex;
     
     thread_params *tp = malloc(sizeof(struct thread_params_struct));
     tp->head = head;
-    tp->map = *dict;
+    tp->map = dict;
 
     return tp;
 }
@@ -54,17 +56,17 @@ void addToList(mutex_map** head, mutex_map** mutex) {
 }
 
 void *cause_a_deadlock(void *param) {
-    thread_params **tp = (thread_params**) param;
-    mutex_map *map = (*tp)->map;
-    mutex_map **head = (*tp)->head;
+    thread_params *tp = (thread_params*) param;
+    mutex_map **map = tp->map;
+    mutex_map **head = tp->head;
     
-    int key = map->key;
-    pthread_mutex_t mutex = map->mutex;
+    int key = (*map)->key;
+    pthread_mutex_t *mutex = (*map)->mutex;
     
     printf("Thread %d started...\n", key);
 
     // lock its own mutex
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(mutex);
 
     printf("Thread %d acquired mutex %d\n", key, key);
 
@@ -78,7 +80,7 @@ void *cause_a_deadlock(void *param) {
         temp = temp->next;
     }
 
-    pthread_mutex_t next_mutex_to_acquire;
+    pthread_mutex_t *next_mutex_to_acquire;
     int mutex_num = 0;
 
     if (temp->next != NULL) {
@@ -92,16 +94,17 @@ void *cause_a_deadlock(void *param) {
         mutex_num = first->key;
     }
 
-    sleep(1);
+    sleep(rand() % 10);
     printf("Thread %d attempting to acquire mutex %d...\n", key, mutex_num);
-    pthread_mutex_lock(&next_mutex_to_acquire);
+    pthread_mutex_lock(next_mutex_to_acquire);
     printf("Thread %d acquired mutex %d\n", key, mutex_num);
     // critical section
     printf("Inside Thread %d Critical Section\n", key);
     // end critical section
 
-    pthread_mutex_unlock(&mutex);
-    pthread_mutex_unlock(&next_mutex_to_acquire);
+    pthread_mutex_unlock(mutex);
+    pthread_mutex_unlock(next_mutex_to_acquire);
+    printf("Thread %d released mutex %d and %d.\n", key, key, mutex_num);
 
     pthread_exit(0);
 }
@@ -115,6 +118,7 @@ void traverseList(mutex_map** head) {
 }
 
 int main() {
+    srand(time(0));
     pthread_t thread_1, thread_2, thread_3, thread_4, thread_5;
     
     mutex_map *first_mutex_map, *second_mutex_map, *third_mutex_map, *fourth_mutex_map, *fifth_mutex_map;
@@ -131,16 +135,13 @@ int main() {
     addToList(&head, &third_mutex_map);
     addToList(&head, &fourth_mutex_map);
     addToList(&head, &fifth_mutex_map);
-
-    // traverseList(&head);
-
     
 
-    pthread_create(&thread_1, NULL, cause_a_deadlock, &first_tp);
-    pthread_create(&thread_2, NULL, cause_a_deadlock, &second_tp);
-    pthread_create(&thread_3, NULL, cause_a_deadlock, &third_tp);
-    pthread_create(&thread_4, NULL, cause_a_deadlock, &fourth_tp);
-    pthread_create(&thread_5, NULL, cause_a_deadlock, &fifth_tp);
+    pthread_create(&thread_1, NULL, cause_a_deadlock, first_tp);
+    pthread_create(&thread_2, NULL, cause_a_deadlock, second_tp);
+    pthread_create(&thread_3, NULL, cause_a_deadlock, third_tp);
+    pthread_create(&thread_4, NULL, cause_a_deadlock, fourth_tp);
+    pthread_create(&thread_5, NULL, cause_a_deadlock, fifth_tp);
 
     pthread_join(thread_1, NULL);
     pthread_join(thread_2, NULL);
