6c68aa5e133a326c3049be23dc0be18aa41b616a
V4vernon <Junfeng98@Hotmail.com>
2021-04-01 13:26:11 +0800

    Added arguments

--- assignment2/Qn1/Qn1.c
+++ assignment2/Qn1/Qn1.c
@@ -9,30 +9,46 @@
 
 
 int verify_child_processes(int pid, int number_of_processes, pid_t pids[]);
-void create_child_processes(int counter, int argc, pid_t pid, char *argv[], pid_t process[]);
-void wait_processes(int child, int status, int argc, pid_t process[], char *argv[],struct tms tms_start,struct tms tms_end);
+void create_child_processes(int counter, int argc, pid_t pid, char *argv[], pid_t process[], char *arguments[]);
+void wait_processes(int child, int status, int argc, pid_t process[], char *argv[], struct tms tms_start, struct tms tms_end);
 
 int main(int argc, char *argv[]) {
 
 	pid_t pid, process[argc];
 	int counter, child, status;
 	struct tms tms_start, tms_end;
+	char *arguments[100];
 
-	/*Validate user input */
+	/*The program ensures that the user enters at least a single command. The first argc belongs to the name of the program
+	  and the second argc belongs to the command. It will exit if it does not match the criteria. */
 	if (argc < 2) {
 		fprintf(stderr, "Usage: ./Qn1 [COMMAND] [COMMAND]...[COMMAND]\n");
-		fprintf(stderr, "\tThis program takes a list of command line arguments and would create N direct child processes. Each child executing one of the N commands concurrently.\n\n");
+		fprintf(stderr, "\tThis program takes a list of command line arguments and would create N direct\n");
+		fprintf(stderr,	"\tchild processes.Each child executing one of the N commands concurrently.\n\n");
 		fprintf(stderr, "\tArguments: \n");
-		fprintf(stderr, "\tCOMMAND: Full Path or relative path of linux command such as /bin/ls, /bin/ps, /bin/date, /bin/who, /bin/uname etc\n");
+		fprintf(stderr, "\tCOMMAND: Full Path or relative path of linux command such as /bin/ls, /bin/ps,\n");
+		fprintf(stderr, "\t/bin/date, /bin/who, /bin/uname etc\n");
 		exit(0);
 	}
+
+	/*This allows the user to key in the arguments for the respective commands that he have entered earlier on. 
+	  Do not put space after keying the arguments or NULL. NULL should be uppercase instead of lowercase. 
+	*/
+	for (counter = 1; counter < argc; counter++) {
+		arguments[counter] = malloc(100);
+		printf("Enter the argument for Command %s (enter NULL if no arguments): ", argv[counter]);
+		fgets (arguments[counter], 100, stdin);
+		arguments[counter][strlen(arguments[counter]) - 1] = '\0';      
+	}
+
+
 	/*Start time*/
 	times(&tms_start);
 	/*Creating child processes */
-	create_child_processes(counter, argc, pid, argv, process);
+	create_child_processes(counter, argc, pid, argv, process, arguments);
 
 	/*Wait for each child processes but not sequentially */
-	wait_processes(child, status, argc, process, argv,tms_start,tms_end);
+	wait_processes(child, status, argc, process, argv, tms_start, tms_end);
 
 	/*All child processes finished executing*/
 	printf("%s", "All done, bye!");
@@ -46,7 +62,7 @@ int main(int argc, char *argv[]) {
    Each child executing one of the N commands concurrently.
 */
 
-void create_child_processes(int counter, int argc, pid_t pid, char *argv[], pid_t process[]) {
+void create_child_processes(int counter, int argc, pid_t pid, char *argv[], pid_t process[], char *arguments[]) {
 	for (counter = 1; counter < argc; counter++) {
 		pid = fork();
 
@@ -55,14 +71,24 @@ void create_child_processes(int counter, int argc, pid_t pid, char *argv[], pid_
 			perror("Forking Child Process Failed\n");
 			exit(EXIT_FAILURE);
 		}
-		/* Executing the child process. If the execution of the child process failed, it
-		   will print out error message and exits the system.
+		/* Executing the child process. If strcmp is not equal to 0, it means that it have argument specified for
+	       the respective commands and thus will run with the argument else it will run without the argument.
+		   If the execution of the child process failed, it will print out error message and exits the system.
 		*/
 		else if (pid == 0) {
-			char *args[] = {argv[counter], NULL};
-			execvp(args[0], args);
-			fprintf(stderr, "Command %s has not completed successfully\n\n", argv[counter]);
-			exit(EXIT_FAILURE);
+			if (strcmp(arguments[counter], "NULL") != 0) {
+				printf("%s",arguments[counter]);
+				char *args[] = {argv[counter], arguments[counter], NULL};
+				execvp(args[0], args);
+				fprintf(stderr, "Command %s has not completed successfully\n\n", argv[counter]);
+				exit(EXIT_FAILURE);
+
+			} else {
+				char *args[] = {argv[counter], NULL};
+				execvp(args[0], args);
+				fprintf(stderr, "Command %s has not completed successfully\n\n", argv[counter]);
+				exit(EXIT_FAILURE);
+			}
 		}
 		/*  Tracks the process that belonged to each child PID by storing it in an array.
 			Element 0 is not in used in this situation
@@ -91,9 +117,11 @@ int verify_child_processes(int pid, int number_of_processes, pid_t pids[]) {
 
 /* To make it concurrent, we cannot wait inside the loop as the child processes would then execute sequentially.
    waitpid(0) means that it will be waiting for any child process whose process group ID is equal to that of the calling process.
- 
+   WIFEXITED(status): child exited normally
+   WEXITSTATUS(status): return code when child exits
+   WIFSIGNALED(status): child exited because a signal was not caught
 */
-void wait_processes(int child, int status, int argc, pid_t process[], char *argv[], struct tms tms_start,struct tms tms_end) {
+void wait_processes(int child, int status, int argc, pid_t process[], char *argv[], struct tms tms_start, struct tms tms_end) {
 	while ((child = waitpid(0, &status, 0)) > 0) {
 		/* End time of each child processes*/
 		times(&tms_end);
@@ -103,8 +131,8 @@ void wait_processes(int child, int status, int argc, pid_t process[], char *argv
 		} else if (WIFEXITED(status)) {
 			if (WEXITSTATUS(status) == 0) {
 				clock_t real = tms_end.tms_cstime - tms_start.tms_stime;
-				float running_time = real/(double)sysconf(_SC_CLK_TCK);
-				printf("Command /bin/%s has completed successfully at time %f\n\n",argv[id],running_time);
+				float running_time = real / (double)sysconf(_SC_CLK_TCK);
+				printf("Command %s has completed successfully at time %f\n\n", argv[id], running_time);
 			}
 		} else if (WIFSIGNALED(status)) {
 			printf("Process %s (PID %d) was signalled %d\n", argv[id], child, WTERMSIG(status));
